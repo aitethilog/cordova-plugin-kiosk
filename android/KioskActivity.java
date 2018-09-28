748af40646f65d0c00e88b607dbac1db914b4163
Antoine DECRIEM <decriem.antoine@gmail.com>
2018-09-28 16:51:06 +0200

    ok

--- android/KioskActivity.java
+++ android/KioskActivity.java
@@ -16,7 +16,7 @@ import android.view.ViewGroup.LayoutParams;
 import android.view.LayoutInflater;
 import android.graphics.PixelFormat;
 import android.view.Gravity;
-import android.R;
+import android.R.*;
 import java.lang.Integer;
 import java.util.Collections;
 import java.util.Set;
@@ -74,7 +74,7 @@ public class KioskActivity extends CordovaActivity {
         statusBarOverlay = StatusBarOverlay.createOrObtainPermission(this);
 
         // disable status bar swipe
-        RelativeLayout mMainLayout = (RelativeLayout) LayoutInflater.from(this).inflate(jk.cordova.plugin.kiosk.R.layout.activity_fullscreen, null);
+        RelativeLayout mMainLayout = (RelativeLayout) LayoutInflater.from(this).inflate(R.layout.activity_fullscreen, null);
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
